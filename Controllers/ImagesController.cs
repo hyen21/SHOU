4381be5382e12044893bbe703a2622b26c6af366
huyenma <huyentay2112@gmail.com>
2023-11-06 18:29:11 +0700

    add CRUD base

---
+++ Controllers/ImagesController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SHOU.Contexts;
+using SHOU.Models;
+
+namespace SHOU.Controllers
+{
+    public class ImagesController : Controller
+    {
+        private readonly SHOUContext _context;
+
+        public ImagesController(SHOUContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Images
+        public async Task<IActionResult> Index()
+        {
+              return _context.Images != null ? 
+                          View(await _context.Images.ToListAsync()) :
+                          Problem("Entity set 'SHOUContext.Images'  is null.");
+        }
+
+        // GET: Images/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null || _context.Images == null)
+            {
+                return NotFound();
+            }
+
+            var image = await _context.Images
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (image == null)
+            {
+                return NotFound();
+            }
+
+            return View(image);
+        }
+
+        // GET: Images/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Images/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,IdUser,ImageUrl")] Image image)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(image);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(image);
+        }
+
+        // GET: Images/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null || _context.Images == null)
+            {
+                return NotFound();
+            }
+
+            var image = await _context.Images.FindAsync(id);
+            if (image == null)
+            {
+                return NotFound();
+            }
+            return View(image);
+        }
+
+        // POST: Images/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("Id,IdUser,ImageUrl")] Image image)
+        {
+            if (id != image.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(image);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ImageExists(image.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(image);
+        }
+
+        // GET: Images/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null || _context.Images == null)
+            {
+                return NotFound();
+            }
+
+            var image = await _context.Images
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (image == null)
+            {
+                return NotFound();
+            }
+
+            return View(image);
+        }
+
+        // POST: Images/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            if (_context.Images == null)
+            {
+                return Problem("Entity set 'SHOUContext.Images'  is null.");
+            }
+            var image = await _context.Images.FindAsync(id);
+            if (image != null)
+            {
+                _context.Images.Remove(image);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ImageExists(string id)
+        {
+          return (_context.Images?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ Controllers/LikesController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SHOU.Contexts;
+using SHOU.Models;
+
+namespace SHOU.Controllers
+{
+    public class LikesController : Controller
+    {
+        private readonly SHOUContext _context;
+
+        public LikesController(SHOUContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Likes
+        public async Task<IActionResult> Index()
+        {
+              return _context.Likes != null ? 
+                          View(await _context.Likes.ToListAsync()) :
+                          Problem("Entity set 'SHOUContext.Likes'  is null.");
+        }
+
+        // GET: Likes/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null || _context.Likes == null)
+            {
+                return NotFound();
+            }
+
+            var like = await _context.Likes
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (like == null)
+            {
+                return NotFound();
+            }
+
+            return View(like);
+        }
+
+        // GET: Likes/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Likes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,IdUser,IdPost")] Like like)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(like);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(like);
+        }
+
+        // GET: Likes/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null || _context.Likes == null)
+            {
+                return NotFound();
+            }
+
+            var like = await _context.Likes.FindAsync(id);
+            if (like == null)
+            {
+                return NotFound();
+            }
+            return View(like);
+        }
+
+        // POST: Likes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("Id,IdUser,IdPost")] Like like)
+        {
+            if (id != like.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(like);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!LikeExists(like.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(like);
+        }
+
+        // GET: Likes/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null || _context.Likes == null)
+            {
+                return NotFound();
+            }
+
+            var like = await _context.Likes
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (like == null)
+            {
+                return NotFound();
+            }
+
+            return View(like);
+        }
+
+        // POST: Likes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            if (_context.Likes == null)
+            {
+                return Problem("Entity set 'SHOUContext.Likes'  is null.");
+            }
+            var like = await _context.Likes.FindAsync(id);
+            if (like != null)
+            {
+                _context.Likes.Remove(like);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool LikeExists(string id)
+        {
+          return (_context.Likes?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
