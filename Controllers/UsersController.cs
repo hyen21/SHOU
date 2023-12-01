6486596b1491a09f2987288157953b1f85f123e5
VietAnhDepZai <vietanh8264@gmail.com>
2023-12-01 17:32:09 +0700

    no message

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -18,16 +18,20 @@ using SHOU.Extentions;
 using System.Text;
 using XSystem.Security.Cryptography;
 using XAct.Users;
+using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Hosting;
 
 namespace SHOU.Controllers
 {
     public class UsersController : Controller
     {
         private readonly SHOUContext _context;
+        private readonly IWebHostEnvironment _webHostEnvironment;
         ProfileEditModel profileEditModel = new ProfileEditModel();
         bool? isDisable;
-        public UsersController(SHOUContext context)
+        public UsersController(SHOUContext context, IWebHostEnvironment webHostEnvironment)
         {
+            _webHostEnvironment = webHostEnvironment;
             _context = context;
         }
 
@@ -220,6 +224,7 @@ namespace SHOU.Controllers
                         new Claim("Id", data.Id),
                         new Claim("UserName", data.UserName),
                         new Claim("Name", data.Name),
+                        new Claim("Avatar", data.Avatar),
                     };
 
                     ClaimsIdentity identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
@@ -354,12 +359,158 @@ namespace SHOU.Controllers
                 user.Email = email;
                 _context.Update(user);
                 await _context.SaveChangesAsync();
+                List<Claim> claims = new List<Claim>()
+                    {
+                        new Claim(ClaimTypes.NameIdentifier, user.UserName),
+                        new Claim("Id", user.Id),
+                        new Claim("UserName", user.UserName),
+                        new Claim("Name", user.Name),
+                        new Claim("Avatar", user.Avatar),
+                    };
+                ClaimsIdentity identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+                AuthenticationProperties authenticationProperties = new AuthenticationProperties()
+                {
+                    AllowRefresh = true,
+                    IsPersistent = true,
+                };
+
+                await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme,
+                    new ClaimsPrincipal(identity), authenticationProperties);
                 return Json(new { code = 200, msg = "Cập nhật thông tin thành công!" });
 
             }
             catch (Exception ex)
             {
                 return Json(new { code = 500, msg = "Cập nhật thông tin thất bại!" });
+            }
+        }
+
+        public async Task<JsonResult> UpdateAvatar(IFormFile avatar)
+        {
+            try
+            {
+                var id = @User.FindFirst("Id")?.Value;
+                var user = await _context.Users.FindAsync(id);
+
+                var uploadsFolder = Path.Combine(_webHostEnvironment.WebRootPath, "avatars");
+                var fileName = Guid.NewGuid().ToString() + Path.GetExtension(avatar.FileName);
+                var filePath = Path.Combine(uploadsFolder, fileName);
+
+                // Copy tệp tin vào thư mục uploads
+                using (var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    await avatar.CopyToAsync(fileStream);
+                }
+
+                // Lưu đường dẫn vào cơ sở dữ liệu
+                user.Avatar = "/avatars/" + fileName;
+
+                _context.Update(user);
+                await _context.SaveChangesAsync();
+
+                List<Claim> claims = new List<Claim>()
+                    {
+                        new Claim(ClaimTypes.NameIdentifier, user.UserName),
+                        new Claim("Id", user.Id),
+                        new Claim("UserName", user.UserName),
+                        new Claim("Name", user.Name),
+                        new Claim("Avatar", user.Avatar),
+                    };
+
+                ClaimsIdentity identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+                AuthenticationProperties authenticationProperties = new AuthenticationProperties()
+                {
+                    AllowRefresh = true,
+                    IsPersistent = true,
+                };
+
+                await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme,
+                    new ClaimsPrincipal(identity), authenticationProperties);
+                return Json(new { code = 200, msg = "Cập nhật avatar thành công!" });
+
+            }
+            catch (Exception ex)
+            {
+                return Json(new { code = 500, msg = "Cập nhật avatar thất bại!" });
+            }
+        }
+
+        public async Task<JsonResult> UpdateBackground(IFormFile avatar)
+        {
+            try
+            {
+                var id = @User.FindFirst("Id")?.Value;
+                var user = await _context.Users.FindAsync(id);
+
+                var uploadsFolder = Path.Combine(_webHostEnvironment.WebRootPath, "backgrounds");
+                var fileName = Guid.NewGuid().ToString() + Path.GetExtension(avatar.FileName);
+                var filePath = Path.Combine(uploadsFolder, fileName);
+
+                // Copy tệp tin vào thư mục uploads
+                using (var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    await avatar.CopyToAsync(fileStream);
+                }
+
+                // Lưu đường dẫn vào cơ sở dữ liệu
+                user.Background = "/backgrounds/" + fileName;
+
+                _context.Update(user);
+                await _context.SaveChangesAsync();
+                return Json(new { code = 200, msg = "Cập nhật ảnh bìa thành công!" });
+
+            }
+            catch (Exception ex)
+            {
+                return Json(new { code = 500, msg = "Cập nhật ảnh bìa thất bại!" });
+            }
+        }
+
+        public async Task<JsonResult> ChangePassword(string oldPass, string newPass, string reNewPass)
+        {
+            try
+            {
+                if (newPass != reNewPass)
+                {
+                    return Json(new { code = 500, msg = "Nhập lại mật khẩu không khớp!" });
+                }
+                byte[] tempOldPass = ASCIIEncoding.ASCII.GetBytes(oldPass);
+                byte[] hasDataOldPass = new MD5CryptoServiceProvider().ComputeHash(tempOldPass);
+                string hasPassOldPass = "";
+
+                foreach (byte item in hasDataOldPass)
+                {
+                    hasPassOldPass += item;
+                }
+
+                var id = @User.FindFirst("Id")?.Value;
+                var user = await _context.Users.FindAsync(id);
+
+                if (hasPassOldPass != user.Password)
+                {
+                    return Json(new { code = 500, msg = "Mật khẩu cũ không đúng!" });
+                }
+
+                byte[] tempNewPass = ASCIIEncoding.ASCII.GetBytes(newPass);
+                byte[] hasDataNewPass = new MD5CryptoServiceProvider().ComputeHash(tempNewPass);
+                string hasPassNewPass = "";
+
+                foreach (byte item in hasDataNewPass)
+                {
+                    hasPassNewPass += item;
+                }
+                user.Password = hasPassNewPass;
+
+                _context.Update(user);
+                await _context.SaveChangesAsync();
+                return Json(new { code = 200, msg = "Đổi mật khẩu thành công!" });
+
+            }
+            catch (Exception ex)
+            {
+                return Json(new { code = 500, msg = "Đổi mật khẩu thất bại!" });
             }
         }
     }
